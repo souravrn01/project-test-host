1d3a35f0d6329dab7dd574f8427fca824940f031
souravrn01 <112268288+souravrn01@users.noreply.github.com>
2022-12-11 11:28:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))
 app.use(logger('dev'))
+
 app.use(cors()) //to connect frontend and backend without any disturbance
 
 
